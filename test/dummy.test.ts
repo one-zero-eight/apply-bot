a8d917cf7b990f97c711c50a298860317260b869
evermake <vladeryabkin@gmail.com>
2023-02-10 03:04:28 +0300

    chore(ci-cd): add QA workflow with related changes

---
+++ test/dummy.test.ts
@@ -0,0 +1,10 @@
+/**
+ * Testing file that was created to not get an error when running `deno test`.
+ * Remove this dummy test file when real tests are added.
+ */
+
+import { assertEquals } from "testing";
+
+Deno.test("2 + 2 = 4", () => {
+  assertEquals(2 + 2, 4);
+});
